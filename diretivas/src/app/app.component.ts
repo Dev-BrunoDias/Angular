05796ff74d37df43e81ce3a873bd35a51eee6bed
BrunoDiasLS <brunodlopes82@globo.com>
2022-10-20 17:58:44 -0300

    finalização projeto cadastro de produtos

--- diretivas/src/app/app.component.ts
+++ diretivas/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Produto } from './interfaces/Produto';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'diretivas';
+  nome: string = ''
+  imagem: string = ''
+  preco: number = 0
+  estoque: number = 0
+  descricao: string =''
+ 
+  produtos: Produto[] = []
+  //array de produtos 
+
+  adicionarProduto(evento: any): void {
+
+    evento.preventDefault()
+    evento.stopPropagation()
+    
+    this.produtos.push({ //objeto sempre dentro de chaves{}
+      descricao: this.descricao,
+      estoque: this.estoque,
+      imagem: this.imagem,
+      nome: this.nome,
+      preco: this.preco
+    })
+    this.apagarInput()
+  }
+  apagarInput(){
+    this.nome = ''
+    this.imagem = ''
+    this.preco = 0
+    this.estoque = 0
+    this.descricao = ''
+  }
 }
