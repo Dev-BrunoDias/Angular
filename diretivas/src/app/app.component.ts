0429d2f59a97bd6a78e5db690f3fba8c67a2a0a7
BrunoDiasLS <brunodlopes82@globo.com>
2022-10-23 14:42:52 -0300

    atualização para deletar o card

--- diretivas/src/app/app.component.ts
+++ diretivas/src/app/app.component.ts
@@ -7,35 +7,47 @@ import { Produto } from './interfaces/Produto';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'diretivas';
+   title = 'diretivas';
+  // p: Produto = {
+  //   nome: 'Xbox',
+  //   descricao: 'Console da Microsoft',
+  //   estoque: 250,
+  //   preco: 4000,
+  //   imagem: 'https://images.unsplash.com/photo-1597872200969-2b65d56bd16b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80'
+  // }
+
   nome: string = ''
   imagem: string = ''
   preco: number = 0
   estoque: number = 0
-  descricao: string =''
- 
+  descricao: string = ''
+
+
   produtos: Produto[] = []
-  //array de produtos 
 
   adicionarProduto(evento: any): void {
-
+    console.log(evento)
     evento.preventDefault()
     evento.stopPropagation()
-    
-    this.produtos.push({ //objeto sempre dentro de chaves{}
+
+    this.produtos.push({
       descricao: this.descricao,
-      estoque: this.estoque,
       imagem: this.imagem,
-      nome: this.nome,
-      preco: this.preco
+      preco: this.preco,
+      estoque: this.estoque,
+      nome: this.nome
     })
-    this.apagarInput()
   }
-  apagarInput(){
-    this.nome = ''
-    this.imagem = ''
-    this.preco = 0
-    this.estoque = 0
-    this.descricao = ''
+
+  excluirProduto(indexExclusao: String){
+    for (const iterator of this.produtos) {
+      if(iterator.nome == indexExclusao){
+        const x = this.produtos.indexOf(iterator)
+        this.produtos.splice(x, 1)
+      }
+    }
   }
+
 }
+// preventDefaut() faz com que o comportamento padrão de um elemento HTML seja anulado.
+// stopPropagation() evita com que a emissão do evento afete outros elementos html.
--- diretivas/src/app/produto-card/produto-card.component.ts
+++ diretivas/src/app/produto-card/produto-card.component.ts
@@ -1,14 +1,14 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Produto } from '../interfaces/Produto';
 
 @Component({
-  selector: 'produto-card',
+  selector: 'app-produto-card',
   templateUrl: './produto-card.component.html',
   styleUrls: ['./produto-card.component.css']
 })
 export class ProdutoCardComponent implements OnInit {
 
-  @Input()  
+  @Input()
   prod: Produto = {
     descricao: '',
     estoque: 0,
@@ -17,22 +17,27 @@ export class ProdutoCardComponent implements OnInit {
     preco: 0
   }
 
-  mostrarProduto: boolean = true
+  @Output() excluindoEvent = new EventEmitter<String>();
 
-  esconderProduto() {
-    this.mostrarProduto =false
+  excluir(indexExclusao: String){
+    this.excluindoEvent.emit(indexExclusao)
   }
 
-  const produto = [];
-  const produtos = this.produto.indexOf();
-  this.produto.splice(index, 1);
+  mostrarProduto: boolean = true
 
- 
+  esconderProduto(): void {
+    if(this.mostrarProduto){
+      this.mostrarProduto = false
+    } else {
+      this.mostrarProduto = true
+    }
+  }
 
- 
-  
-  
-  
+  // showProduto(): void {
+  //   if(!this.mostrarProduto) {
+  //     this.mostrarProduto = true
+  //     }
+  // }
 
   constructor() { }
 
@@ -40,3 +45,4 @@ export class ProdutoCardComponent implements OnInit {
   }
 
 }
+
