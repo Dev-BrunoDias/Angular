aa2daa307cbc122cda17ed5c7652f014106eac63
BrunoDiasLS <brunodlopes82@globo.com>
2022-10-27 20:47:56 -0300

    Atualização cadastro

--- forms/src/app/reactive/reactive.component.ts
+++ forms/src/app/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -8,39 +8,81 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class ReactiveComponent implements OnInit {
 
-  /***
-   * Reactive Forms possui estruturas para representar o formulário
+  /**
+   * Reactive Forms possui estruturas
+   * para representar o formulário
    * dentro do TypeScript
    */
 
   /**
-   * FormControl representa um elemento de seu formulário
+   *
+   * FormControl representa um elemento do seu formulário
    */
   nome: FormControl = new FormControl('')
   email: FormControl = new FormControl('')
   senha: FormControl = new FormControl('')
 
-
   /**
-   * FormGroup é uma estrutura do Reactive Forms que permite agruparmos forms controls dentro dela,
-   *  para que o acesso aos valores seja mais facil
+   * FormGroup é uma estrutura do Reactive Forms
+   * que permite agruparmos forms controls dentro
+   * dela, para que o acesso aos valores seja mais
+   * simples
    */
-  dadosForm: FormGroup = new FormGroup({
-    nome: new FormControl(''),
-    email: new FormControl(''),
-    senha: new FormControl('')
+
+  /*dadosForm: FormGroup = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', [Validators.email, Validators.required ]),
+    senha: new FormControl(''),
+  endereco: new FormGroup({
+    cep: new FormControl(''),
+    rua: new FormControl(''),
+    complemento: new FormControl(''),
+    numero: new FormControl('')
+    })
+  })
+  */
+
+  dadosForm: FormGroup = this.fb.group({
+    nome: ['', [Validators.required, Validators.minLength(4)]],
+    email: [''],
+    senha: [''],
+    endereco: this.fb.group({
+      cep: [''],
+      rua: [''],
+      complemento: [''],
+      numero: ['']
+    }),
+    telefones: this.fb.array([
+      ['']
+      
+    ])
   })
 
-  constructor() { }
+  tels = this.dadosForm.get('telefones') as FormArray
+
+  /**
+   * Form Builder -> Objeto que permite criar FormControls, FormGroups ou FormArrays com uma 
+   * sintaxe menor
+   */
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
   }
 
   enviar() {
+    /*
+      A propriedade 'value' do formControl possui
+      o valor que foi digitado dentro do campo do
+      formulário
+    */
 
-    console.log(this.nome.value)
-    console.log(this.nome.value)
-    console.log(this.nome.value)
+    console.log(this.dadosForm.value)
   }
 
+  adicionarCampoDeTelefone(){
+    this.tels.push(new FormControl(''))
+  }
 }
